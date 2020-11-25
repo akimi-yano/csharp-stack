b7753aab32d580f8e0bda411df62ac1df6f7dd52
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-11-24 23:00:54 -0800

    adding text101 games

---
+++ games/text101/Text101/Assets/Scripts/AdventureGames.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGames : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    //int[] oddNumbers = { 1, 3, 5, 7, 9 };
+    //string[] daysOfTheWeek = { "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun" };
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        //Debug.Log(oddNumbers[3]);
+        //Debug.Log(daysOfTheWeek[1]);
+        //textComponent.text = ("I am added programatically");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        */
+
+
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ games/text101/Text101/Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    // first one is mininum ; second one is where it starts to scroll
+    [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+    public string GetStateStory() 
+    { 
+        return storyText; 
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+}
