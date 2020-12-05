64e21202d7f75d53a25b7c10b7760ab1b7858445
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-12-05 00:19:25 -0800

    moq

---
+++ csharp_tdd/Polymorphism/PolymorphismTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PolymorphismTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PolymorphismTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44a5ab96-d020-462f-add1-e17f3d70f703")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp_tdd/Polymorphism/PolymorphismTestProject/UnitTest1.cs
@@ -0,0 +1,128 @@
+﻿
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polymorphism;
+using System;
+
+namespace PolymorphismTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursReturns2800Dollars()
+        {
+            // Arrange
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = 40 * wage;
+            Employee e = new Employee();
+
+            string expectedResponse = $"This ANGRY EMPLOYEE worked {weeklyHours} hrs. " +
+                  $"Paid for 40 hrs at $ {wage}" +
+                  $"/hr = ${salary}";
+
+
+            // Act
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+
+            // Assert 
+            Assert.AreEqual(response, expectedResponse);
+        }
+
+
+        [TestMethod]
+        public void CalculateWeeklySalaryForContractorTest_70wage55hoursReturns3850Dollars()
+        {
+            // Arrange
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = weeklyHours * wage;
+            Contractor e = new Contractor();
+
+            string expectedResponse = $"This HAPPY CONTRACTOR worked {weeklyHours} hrs. " +
+                              $"Paid for {weeklyHours} hrs at $ {wage}" +
+                              $"/hr = ${salary} ";
+
+
+            // Act
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+
+            // Assert 
+            Assert.AreEqual(response, expectedResponse);
+        }
+
+
+
+        [TestMethod]
+        public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursDoesNotReturnCorrectString()
+        {
+            // Arrange
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = 40 * wage;
+            Employee e = new Employee();
+
+            string expectedResponse = $"Problem - 1 : This ANGRY EMPLOYEE worked {weeklyHours} hrs. " +
+                  $"Paid for 40 hrs at $ {wage}" +
+                  $"/hr = ${salary}";
+
+
+            // Act
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+
+            // Assert 
+            Assert.AreNotEqual(response, expectedResponse);
+        }
+
+
+        [TestMethod]
+        public void CalculateWeeklySalaryForContractorTest_70wage55hoursDoesNotReturnCorrectString()
+        {
+            // Arrange
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = weeklyHours * wage;
+            Contractor e = new Contractor();
+
+            string expectedResponse = $"Problem - 2 : This HAPPY CONTRACTOR worked {weeklyHours} hrs. " +
+                              $"Paid for {weeklyHours} hrs at $ {wage}" +
+                              $"/hr = ${salary} ";
+
+
+            // Act
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+
+            // Assert 
+            Assert.AreNotEqual(response, expectedResponse);
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
