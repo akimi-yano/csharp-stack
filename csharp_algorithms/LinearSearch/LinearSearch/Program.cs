2e480196d7836a34a073c2dc366ed5feb8b15da0
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-12-07 11:16:13 -0800

    linear

---
+++ csharp_algorithms/LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theValue = 7;
+            int[] array = new int[] { 1, 4, 5, 7, 9, 22 };
+
+            Console.WriteLine("Our array contsins:");
+            Array.ForEach(array, x => Console.Write(x + " "));
+            Console.WriteLine($"\n\nThe result of a linear search " +
+                $"for {theValue} is: ");
+            Console.WriteLine(linearSearch(array, theValue));
+        }
+
+        static int linearSearch(int[] a, int x)
+        {
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == x)
+                {
+                   return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
